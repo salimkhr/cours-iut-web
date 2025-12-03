939a52b5baaaf739ea3b6d3bd4e729d02b466a04
salimkhr <salimkhr@gmaail.com>
2025-12-03 09:47:53 +0100

    Refactor TP component in PHP course: update "Admin Access" section by removing `delete()` method from the `checkAdminAccess()` requirement for improved clarity.

--- src/components/page/CoursesSection.tsx
+++ src/components/page/CoursesSection.tsx
@@ -9,14 +9,14 @@ interface CoursesSectionProps {
 export default function CoursesSection({
                                            title,
                                            children,
-                                           containerClassName="grid gap-6 lg:gap-8 w-full p-4 lg:px-40 mx-auto grid-cols-[repeat(auto-fit,minmax(400px,1fr))]"
+                                           containerClassName="grid gap-6 lg:gap-8 w-full p-4 lg:px-40 mx-auto grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(400px,1fr))]"
                                        }: CoursesSectionProps) {
     return (
-        <section className="w-full px-4 lg:px-8  z-100">
+        <section className="w-full px-4 lg:px-8 z-100 overflow-x-hidden">
             <h2 className="text-4xl lg:text-6xl font-extrabold mb-4 lg:mb-8 text-center opacity-0 animate-fade-in-up">
                 {title}
             </h2>
-            <div className={containerClassName}>{children}</div>
+            <div className={`${containerClassName} w-full`}>{children}</div>
         </section>
     );
 }
