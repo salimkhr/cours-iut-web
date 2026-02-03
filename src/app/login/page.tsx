33305fe323679230294f13d351a57ddfb2d01a1c
salimkhr <salimkhr@gmaail.com>
2026-02-03 20:48:28 +0100

    Refactor: Adjust padding class in LoginPage for consistent responsive design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/login/LoginForm";
 export default async function LoginPage() {
 
     return (
-        <div className="flex items-center justify-center p-4 pt-50 header-login">
+        <div className="flex items-center justify-center p-4 lg:pt-20 header-login">
             <LoginForm/>
         </div>
     );
