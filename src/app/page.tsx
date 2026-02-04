60f4237049fbf5420abe0b2f0aefc326a85cb6a0
salimkhr <salimkhr@gmaail.com>
2026-02-04 18:38:44 +0100

    Fix: Correct branch name typo in GitHub Actions workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,34 @@ import getModules from "@/lib/getModules";
 import HeroSection from "@/components/page/HeroSection";
 import CoursesSection from "@/components/page/CoursesSection";
 import PageFooter from "@/components/page/PageFooter";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Développement Web | Apprenez le front-end et le back-end",
+    description: "Maîtrisez les bases du développement web front-end et back-end avec nos cours interactifs et complets.",
+    openGraph: {
+        title: "Développement Web | Apprenez le front-end et le back-end",
+        description: "Maîtrisez les bases du développement web front-end et back-end avec nos cours interactifs et complets.",
+        url: "https://salimkhraimeche.dev", // Remplace par l'URL de ton site
+        siteName: "Salim Khraimeche",
+        images: [
+            {
+                url: "https://salimkhraimeche.dev/images/header.svg",
+                width: 1200,
+                height: 630,
+                alt: "Développement Web",
+            },
+        ],
+        locale: "fr_FR",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Développement Web | Apprenez le front-end et le back-end",
+        description: "Maîtrisez les bases du développement web front-end et back-end avec nos cours interactifs et complets.",
+        images: ["https://salimkhraimeche.dev/images/header.svg"],
+    },
+};
 
 export default async function Home() {
     const modules = await getModules();
