09344ea65020cf576a3172108842a6d10622b28a
salimkhr <salimkhr@gmaail.com>
2026-02-07 11:42:08 +0100

    Refactor: Remove redundant `baseURL` and `secret` configs from better-auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,8 +5,6 @@ import {admin} from "better-auth/plugins";
 
 export const auth = betterAuth({
     database: mongodbAdapter(await connectToDB()),
-    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-    secret: process.env.BETTER_AUTH_SECRET || "static_secret_for_build",
     emailAndPassword: {
         enabled: true,
         minPasswordLength: 1,
